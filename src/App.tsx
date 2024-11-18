9fa454008093a70ce79d94ee5ee6e1e5df9ef2ec
rileymanda <rileymanda69@gmail.com>
2024-11-18 05:03:42 +0200

    Implemented header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ApplicationPage from "./pages/ApplicationPage";
 import LandingPage from "./pages/LandingPage";
 import SummaryPage from "./pages/SummaryPage";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/application" element={<ApplicationPage />} />
