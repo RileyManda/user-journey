2dea894f4cfad6121fb4d20689ecb2625331d8d9
rileymanda <rileymanda69@gmail.com>
2024-11-19 23:56:31 +0200

    Added missing depednency-redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App.tsx";
 import { ThemeProvider } from "@mui/system";
 import theme from "./theme/theme.ts";
 import { CssBaseline } from "@mui/material";
+import store from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );
