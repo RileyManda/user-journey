fc255e9221336c3a1b1995bb6a665da718e5241c
rileymanda <rileymanda69@gmail.com>
2024-11-19 13:39:36 +0200

    Wrapped summary page in global container|Implemented checklist section

--- src/pages/SummaryPage.tsx
+++ src/pages/SummaryPage.tsx
@@ -1,8 +1,21 @@
+import ChecklistSection from "../components/ChecklistSection";
+import GlobalContainer from "../components/GlobalContainer";
+import SummaryPageTitle from "../components/SummaryTitle";
+
 const SummaryPage = () => {
-    return (
-        <div>
-            <h1>Summary Page</h1>
-        </div>
-    );
+  const handleContinue = () => {
+    console.log("Continue button clicked!");
+  };
+  return (
+    <GlobalContainer>
+      <SummaryPageTitle
+        title="Your Application Checklist"
+        subtitle="Complete the following to submit an application"
+        buttonText="Continue Application"
+        onButtonClick={handleContinue}
+      />
+      <ChecklistSection isSummary={true} />
+    </GlobalContainer>
+  );
 };
 export default SummaryPage;
