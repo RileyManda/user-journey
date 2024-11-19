bce3eea28afc33c0454de15a2c656fd21767f0b9
rileymanda <rileymanda69@gmail.com>
2024-11-19 17:19:12 +0200

    Passing filtered selected items from checkbox change

--- src/pages/SummaryPage.tsx
+++ src/pages/SummaryPage.tsx
@@ -1,21 +1,36 @@
+import { useLocation, useNavigate } from "react-router";
 import ChecklistSection from "../components/ChecklistSection";
 import GlobalContainer from "../components/GlobalContainer";
 import SummaryPageTitle from "../components/SummaryTitle";
 
 const SummaryPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // Get selected items from location state
+  const selectedItems = location.state?.selectedItems || {};
+
   const handleContinue = () => {
     console.log("Continue button clicked!");
+    // Navigate to the next step or final page
+    navigate("/final-step");
   };
+
   return (
     <GlobalContainer>
       <SummaryPageTitle
         title="Your Application Checklist"
-        subtitle="Complete the following to submit an application"
+        subtitle="Review your selections before proceeding"
         buttonText="Continue Application"
         onButtonClick={handleContinue}
       />
-      <ChecklistSection isSummary={true} />
+      <ChecklistSection
+        isSummary={true} // Summary mode enabled
+        selectedItems={selectedItems} // Pass filtered selected items
+        onCheckboxChange={() => {}} // Disable checkbox interaction
+      />
     </GlobalContainer>
   );
 };
+
 export default SummaryPage;
